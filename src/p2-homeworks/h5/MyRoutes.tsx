54ecd1c966f8bfd50d2b2a15f48b2b33edabefd4
Bogdan <bogdan_mykhailov@icloud.com>
2022-03-10 23:40:31 +0200

    finish hw5

--- src/p2-homeworks/h5/MyRoutes.tsx
+++ src/p2-homeworks/h5/MyRoutes.tsx
@@ -29,7 +29,7 @@ function MyRoutes() {
         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
         {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-        <Route path='/*' element={<Error404/>}/>
+        <Route path='*' element={<Error404/>}/>
 
       </Routes>
     </div>
