918b206ad9e51747c550a0b0e1c724cc8f17429b
Bogdan <bogdan_mykhailov@icloud.com>
2022-04-05 22:17:59 +0300

    finish hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,34 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import {UserType} from "../HW8";
+
+type ActionType = CheckType | SortType
+
+type CheckType = {
+  type: string
+  payload: number
+}
+type SortType = {
+  type: string
+  payload: 'up' | 'down'
+}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      const newState = [...state].sort((a, b) => {
+        if (a.name > b.name) {
+          return (1)
+        } else if (a.name < b.name) {
+          return (-1)
+        } else return 0
+      })
+
+      return action.payload === 'up' ? newState : newState.reverse()
     }
+    case 'check': {
+
+      return state.filter(s => s.age >= action.payload)
+    }
+    default:
+      return state
+  }
 }
